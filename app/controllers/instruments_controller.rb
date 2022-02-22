559479162ac0dafec2b3e1360f0c7ed02b1a2554
Matthieu Villeret <matthieuvilleret@yahoo.fr>
2022-02-22 15:42:16 +0100

    image upload to cloudinary on new instrument form

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -10,6 +10,7 @@ class InstrumentsController < ApplicationController
 
   def create
     @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
     if @instrument.save
       redirect_to root_path
     else
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,6 @@
 class Instrument < ApplicationRecord
   CATEGORY = ['Strings', 'Keyboard', 'Woodwind', 'Brass', 'Percussions', 'Amplification', 'Microphones', 'Cables & Accessories'].freeze
-  belongs_to :user
+  belongs_to :user # 1/ creates a validation, instrument.user needs to exist! 2/ Gives access to instrument.users
   has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
@@ -8,5 +8,4 @@ class Instrument < ApplicationRecord
   validates :location, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
   validates :price, presence: true, uniqueness: true, numericality: { only_integer: true }
-  validates :rating, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
