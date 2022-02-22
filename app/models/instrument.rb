fb6a46d7e8897c5d013d52bd88c764ad1668f0b3
Lilia Bedidi <lilia.bedidi@gmail.com>
2022-02-22 11:48:38 +0100

    refacto for CATEGORY array + better category names

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,7 @@
 class Instrument < ApplicationRecord
+  CATEGORY = ['Strings', 'Keyboard', 'Woodwind', 'Brass', 'Percussions', 'Amplification', 'Microphones', 'Cables & Accessories'].freeze
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :category, presence: true, uniqueness: true, inclusion: { in: Instrument::CATEGORY }
 end
