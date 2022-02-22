75caeb9cfabef640068a49fd5a1da37f39aad97b
Lilia Bedidi <lilia.bedidi@gmail.com>
2022-02-22 10:58:16 +0100

    add name to instruments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+CATEGORY = ['wind instruments', 'drums', 'guitars', 'keyboards']
+## For instruments: ##
+40.times do
+  instru = Instrument.new(
+    name: Faker::Music.instrument,
+    category: CATEGORY.sample,
+    location: Faker::Address.city,
+    rating: [1,2,3,4,5].sample,
+    description: Faker::Lorem.sentence,
+    price: rand(330)
+  )
+  p instru.save
+end
+
+40.times do
+  User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    rating: [1,2,3,4,5].sample,
+    bio: Faker::Lorem.sentence
+  )
+end
