1ddaf40d3d558e6618d0d7e896ba419eb14f0ec7
marjorie aubert <aubertmarjorie2@gmail.com>
2022-02-23 12:06:41 +0100

    policies booking, controller booking, partial et view de new

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :instruments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :instruments
+  resources :bookings
 
 end
