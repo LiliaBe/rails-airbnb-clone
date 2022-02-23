0b8f3f9c59d1bc5c80ee0e736858b48532935f82
Stephen Grieves <stephengrieves@gmail.com>
2022-02-23 11:34:36 +0100

    show created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :instruments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :instruments
-
 end
